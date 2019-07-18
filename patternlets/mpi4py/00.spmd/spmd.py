9a67a82118cd1d20ce868ec2054ff3b21fece47f
Libby Shoop <shoop@macid28154.home>
2019-07-17 22:37:00 -0500

    added python script to execute mpirun

--- patternlets/mpi4py/00.spmd/spmd.py
+++ patternlets/mpi4py/00.spmd/spmd.py
@@ -1,7 +1,23 @@
-#
-#
+# spmd.py
 # Multiple processes will run this same program concurrently,
 # each creating different output data.
+# Illustrates the single program multiple data (SPMD) pattern
+# using basic MPI commands.
+#
+#  Libby Shoop, Macalester College, July 2019
+#
+# Example usage:
+#      python run.py 4
+#  Here the 4 signifies the number of processes to start up in mpi.
+#  run.py executes this program within mpirun using
+#         the number of processes given.
+#
+# Exercise:
+#    - Compare source code to output.
+#    - Rerun, using varying numbers of processes, e.g. 2, 8, 16
+#    (i.e., vary the last argument to run.py).
+#    - Explain what "multiple data" values this
+#    "single program" is generating.
 #
 
 from mpi4py import MPI
