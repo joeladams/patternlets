684c2c7e43b0c4c00c32b3f9f1fc3f66d79dccb6
Libby Shoop <shoop@macid28154.home>
2019-07-18 00:17:29 -0500

    simplified mpi4py to remove directory structure and use one run.py file for all patternlets.

---
+++ patternlets/mpi4py/01masterWorker.py
@@ -0,0 +1,33 @@
+# masterWorker.py
+# Multiple processes will run this same program concurrently,
+# each creating different output data.
+# Illustrates the master-worker pattern
+# using basic MPI commands.
+#
+#  Libby Shoop, Macalester College, July 2019
+#
+# Example usage:
+#      python run.py 4
+#  Here the 4 signifies the number of processes to start up in mpi.
+#  run.py executes this program within mpirun using
+#         the number of processes given.
+#
+# Exercise:
+#    - Compare source code to output.
+#    - Rerun, using varying numbers of processes from 1 through 8
+#    (i.e., vary the last argument to run.py).
+#    - Explain what stays the same and what changes as the
+#    number of processes changes.
+#
+
+from mpi4py import MPI
+
+comm = MPI.COMM_WORLD
+id = comm.Get_rank()            #number of the process running the code
+numProcesses = comm.Get_size()  #total number of processes running
+myHostName = MPI.Get_processor_name()  #machine name running the code
+
+if id == 0:
+    print("Greetings from the master, {} of {} on {}".format(id, numProcesses, myHostName))
+else:
+    print("Greetings from a worker, {} of {} on {}".format(id, numProcesses, myHostName))
---
+++ patternlets/mpi4py/run.py
@@ -0,0 +1,37 @@
+#
+# Runs the spmd.py program with mpirun in the shell.
+#
+# Example usage:
+#      python run.py program.py 4
+#  here the 4 signifies the number of processes to start up in mpi.
+#  and program.py is the name of an mpi4py program.
+#
+import sys
+import subprocess
+import os.path
+from os import path
+
+# Check if there are not enough arguments
+if (len(sys.argv) < 3):
+    print("You need more arguments. Usage:")
+    print("   python ./run.py prog.py N [opt]")
+    print("where prog.py should be an mpi4py python program and")
+    print("      N is the number of processes to use and")
+    print("     [opt] is an optional argument for the program if needed (rare)")
+    sys.exit(0)
+
+if (path.exists("./cluster_nodes")):
+    print("using ./cluster_nodes file for names of machines")
+    command = "mpirun -np " + sys.argv[2] + " -hostfile ./cluster_nodes -map-by node python " + sys.argv[1]
+else:
+    command = "mpirun -np " + sys.argv[2] + " python " + sys.argv[1]
+
+# Need to check for existence of a 3rd argument and add it
+if (len(sys.argv) == 4):
+    command = command + sys.argv[3]
+
+print("Command that will run is:")
+print(command)
+print("")
+print("Results:")
+subprocess.call(command, shell=True)
