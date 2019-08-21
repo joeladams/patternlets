c0e63b94f15b619a12e0b3f8625bd9e4494ffc0f
Libby Shoop <shoop@macalester.edu>
2019-08-21 11:44:40 -0500

    add scatter,gather

---
+++ patternlets/mpi4py/14scatter.py
@@ -0,0 +1,65 @@
+#
+#    Illustrates scatter list of lists of computed values created
+#    in the master process.
+#
+#  Libby Shoop, Macalester College, July 2019
+#
+# Example usage:
+#      python run.py ./14scattter.py N
+#  Here the N signifies the number of processes to start up in mpi.
+#
+#  run.py executes this program within mpirun using
+#         the number of processes given.
+#
+# Exercise:
+#
+#    - Run, using N = from 2 through 8 processes.
+#    - Explain to yourself what genListofLists does.
+#    - Visualize what the comm.scatter function is doing.
+#
+
+from mpi4py import MPI
+
+# Create a list of lists to be scattered.
+def genListOfLists(numElements):
+    data = [[0]*3 for i in range(numElements)]
+    for i in range(numElements):
+        #make small lists of 3 distinct elements
+        smallerList = []
+        for j in range(1,4):
+            smallerList = smallerList + [(i+1)*j]
+        # place the small list in the larger list
+        data[i] = smallerList
+    return data
+
+def main():
+    comm = MPI.COMM_WORLD
+    id = comm.Get_rank()            #number of the process running the code
+    numProcesses = comm.Get_size()  #total number of processes running
+    myHostName = MPI.Get_processor_name()  #machine name running the code
+
+    # in mpi4py, the lowercase scatter method only works on lists whose size
+    # is the total number of processes.
+    numElements = numProcesses      #total elements in list created by master process
+
+    # however, the list can contain lists, like this list of 3-element lists,
+    # for example this list of four 3-element lists:
+    #     [[1, 2, 3], [2, 4, 6], [3, 6, 9], [4, 8, 12]]
+
+    if id == 0:
+        data = genListOfLists(numElements)
+        print("Master {} of {} on {} has created list: {}".format(id, numProcesses, myHostName, data))
+    else:
+        data = None
+        print("Worker Process {} of {} on {} starts with {}".format(id, numProcesses, myHostName, data))
+
+    #scatter one small list in the large list on node 0 to each of the processes
+    result = comm.scatter(data, root=0)
+
+    print("Process {} of {} on {} has result after scatter {}".format(id, numProcesses, myHostName, result))
+
+    if id == 0:
+        print("Master {} of {} on {} has original list after scatter: {}".format(id, numProcesses, myHostName, data))
+
+########## Run the main function
+main()
---
+++ patternlets/mpi4py/15gather.py
@@ -0,0 +1,52 @@
+
+#    Illustrates scatter list of lists of computed values created
+#    in the master process.
+#
+#  Libby Shoop, Macalester College, July 2019
+#
+# Example usage:
+#      python run.py ./15scattter.py N
+#  Here the N signifies the number of processes to start up in mpi.
+#
+#  run.py executes this program within mpirun using
+#         the number of processes given.
+#
+# Exercise:
+#
+#    - Run, using N = from 2 through 8 processes.
+#    - Try with different values of SMALL_LIST_SIZE, perhaps changing printing
+#      of result for readability
+#    - Visualize what the comm.gather function is doing.
+#
+
+from mpi4py import MPI
+
+SMALL_LIST_SIZE = 3
+
+# create a small list whose values contain id times multiples of 10
+def genSmallList(id):
+    smallerList = []
+    for j in range(1, SMALL_LIST_SIZE+1):
+        smallerList = smallerList + [(id * 10)*j]
+    return smallerList
+
+def main():
+    comm = MPI.COMM_WORLD
+    id = comm.Get_rank()            #number of the process running the code
+    numProcesses = comm.Get_size()  #total number of processes running
+    myHostName = MPI.Get_processor_name()  #machine name running the code
+
+    #all processes create small lists
+    sendData = genSmallList(id)
+    print("Process {} of {} on {} starts with {}".format(id, numProcesses, myHostName, sendData))
+
+    # gather the small lists at the master node:
+    # final result is a list whose length == the number of processes
+    result = comm.gather(sendData, root=0)
+
+    # only the master node has all of the small lists
+    if id == 0:
+        print("Process {} of {} on {} has result after gather {}".format(id, numProcesses, myHostName, result))
+
+########## Run the main function
+main()
